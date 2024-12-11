ff44c439a91d7dfb85a2283d2ba86377bf1eddd4
kite-machine <elementary38360@gmail.com>
2024-12-11 14:16:12 +0100

    debut bdd

--- firewall/firewall_rules.sh
+++ firewall/firewall_rules.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# Journaliser les connexions acceptées et refusées
+#iptables -A INPUT -p icmp --icmp-type echo-request -j LOG --log-prefix "PING: "
+#iptables -A OUTPUT -p icmp --icmp-type echo-reply -j LOG --log-prefix "PING: "
+#iptables -A FORWARD -j LOG --log-prefix "ACCEPT: " --log-level 4
+#iptables -A FORWARD -j LOG --log-prefix "REJECT: " --log-level 4
+
 # Règles pour la banque centrale et le ministre(exception)
 iptables -A FORWARD -s 192.168.0.3 -j ACCEPT
 iptables -A FORWARD -s 192.168.0.4 -j ACCEPT
